9110506ade3955a34ac8262e745397952fc507a3
dddotcom <dddotcom@gmail.com>
2017-01-20 20:05:01 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,77 @@
+var geoButton = document.querySelector('.geolocation-button');
+geoButton.addEventListener('click', geoFindMe);
+
+function geoFindMe() {
+  var output = document.getElementById("out");
+
+  if (!navigator.geolocation){
+    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+    return;
+  }
+
+  function success(position) {
+    var latitude  = position.coords.latitude.toString().split(".")[0];
+    var longitude = position.coords.longitude.toString().split(".")[0];
+    output.innerHTML = 'Woof!';
+    var myUrl = "http://104.198.245.168:3000/weather?lat=" + latitude + "&lon=" + longitude;
+    console.log(myUrl);
+    getWeather(myUrl);
+  }
+
+  function error() {
+    output.innerHTML = "Unable to retrieve your location";
+  }
+
+  output.innerHTML = "<p>Locating…</p>";
+
+  navigator.geolocation.getCurrentPosition(success, error);
+}
+
+
+$( document ).ready(function() {
+  $('.location-button').click(function(event) {
+    event.preventDefault();
+    var zipCode = $('.zipcode-input').val();
+    var myUrl = "http://104.198.245.168:3000/weather?zip=" + zipCode + ",us";
+    getWeather(myUrl);
+  });
+
+  $('.fav-location').click(function(event) {
+    event.preventDefault();
+    var cityId = $( this ).attr( "data-city-id" );
+    console.log(cityId);
+    var myUrl = "http://104.198.245.168:3000/weather?id=" + cityId;
+    getWeather(myUrl);
+  });
+
+  geoFindMe();
+
+});
+
+function getWeather(myUrl) {
+  $.ajax({
+    method: "GET",
+    url: myUrl,
+  })
+    .done(function( weatherData ) {
+      // console.log( JSON.stringify(weatherData, null, 2) );
+      $('.name').text(weatherData.name);
+      $('.description').text(weatherData.weather[0].description);
+      $('.wind').text(weatherData.wind.speed);
+      $('.temp').text(weatherData.main.temp);
+      $('.humidity').text(weatherData.main.humidity);
+
+      //update images
+      var iconId = weatherData.weather[0].icon;
+
+      $('.yoshi-image').attr("src", 'images/yoshi/' + iconId + '.png');
+      $('body').css('background-image', 'url(images/bg/' + iconId + '.jpg)');
+
+      if(iconId === '13d' || iconId === '13n'){
+        console.log('addClass');
+        $('.yoshi-image').addClass('snow');
+      } else {
+        $('.yoshi-image').removeClass('snow');
+      }
+    });
+}
